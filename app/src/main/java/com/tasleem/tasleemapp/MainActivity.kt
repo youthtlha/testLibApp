e561b61863f065432d6b20a55a35cec6f2f65a9f
Talha Rasool <talharasool700@gmail.com>
2024-07-28 17:12:45 +0500

    Initial commit

---
+++ app/src/main/java/com/tasleem/tasleemapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tasleem.tasleemapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tasleem.tasleemapp.ui.theme.TasleemAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TasleemAppTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Fribe APP Tesst",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TasleemAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ mylibrary/src/main/java/com/tasleem/mylibrary/ToasterView.kt
@@ -0,0 +1,5 @@
+package com.tasleem.mylibrary
+
+class  ToasterView{
+
+}
