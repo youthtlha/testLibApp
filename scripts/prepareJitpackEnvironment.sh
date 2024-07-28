10e6b3abccd225d3c44381f8ba0225a8a8d8210e
Talha Rasool <talharasool700@gmail.com>
2024-07-28 17:43:16 +0500

    Jit fix

---
+++ scripts/prepareJitpackEnvironment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Example environment setup
+echo "Setting up Jitpack environment..."
+
+# Ensure correct Java version is used
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Any other environment setup steps
+
